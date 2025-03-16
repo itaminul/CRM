633284a124012e4382a2994112024f2a1acea163
aminul@atilimited.net <aminul@atilimited.net>
2025-03-16 15:54:24 +0600

    error message

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { ExceptionsFilter } from 'src/filters/all-exceptions';
 
 @Controller('auth')
 export class AuthController {
   constructor(public readonly authService: AuthService) {}
 
   @Post('login')
+  @UseFilters(new ExceptionsFilter())
   async login(@Body() loginDto: LoginDto) {
     try {
       const results = await this.authService.login(loginDto);
